388e61be7e7540a4ec03d9614c58f6a5e0becec9
sodoku <philip.d.stewart@gmail.com>
2025-08-30 15:11:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# tildagon-avatar
+
+Displays the github avatar of your username.
+
+Press the CONFIRM button to initialy download (or redownload) your github avatar.
+
+NOTE: It will use the name from settings, if your set name is not your github username, this will currently not work.
---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+import sys
+
+import app
+import requests
+import settings
+from app_components import Notification, clear_background
+from events.input import BUTTON_TYPES, Buttons
+
+if sys.implementation.name == "micropython":
+    apps = os.listdir("/apps")
+    path = ""
+    for a in apps:
+        if a == "sodoku_tildagon_avatar":
+            path = "/apps/" + a
+    ASSET_PATH = path + "/assets/"
+else:
+    ASSET_PATH = "apps/avatar/"
+
+
+class Avatar(app.App):
+    def __init__(self):
+        self.notification = None
+        self.button_states = Buttons(self)
+        self.name = settings.get("name") or "npentrel"
+        self.image_path = ASSET_PATH + self.name + ".png"
+        self.url = "https://github.com/" + self.name + ".png"
+        self.image_exists = False
+
+    def update(self, delta):
+        if os.path.isfile(self.image_path):
+            self.image_exists = True
+        else:
+            self.image_exists = False
+
+        if self.button_states.get(BUTTON_TYPES["CANCEL"]):
+            self.button_states.clear()
+            self.minimise()
+        if self.button_states.get(BUTTON_TYPES["CONFIRM"]):
+            self.download_avatar()
+
+        if self.notification:
+            self.notification.update(delta)
+
+    def draw(self, ctx):
+        if self.image_exists:
+            clear_background(ctx)
+            ctx.save()
+            ctx.image(self.image_path, -120, -120, 240, 240)
+            ctx.restore()
+        else:
+            ctx.save()
+            ctx.rgb(0.2, 0, 0).rectangle(-120, -120, 240, 240).fill()
+            ctx.font_size = 18
+            ctx.rgb(1, 0, 0).move_to(-80, 0).text("No avatar found")
+            ctx.font_size = 14
+            ctx.rgb(1, 0, 0).move_to(-80, 20).text("Press CONFIRM to download")
+            ctx.rgb(1, 0, 0).move_to(-80, 40).text("Github username: " + self.name)
+            ctx.restore()
+
+        if self.notification:
+            self.notification.draw(ctx)
+
+    def download_avatar(self):
+        print("Downloading " + self.url)
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            with open(self.image_path, "wb") as file:
+                file.write(response.content)
+                print("Sucessfully downloaded " + self.url)
+        else:
+            self.notification = Notification("Failed")
+            print("Could not download " + self.url)
+
+
+__app_export__ = Avatar
